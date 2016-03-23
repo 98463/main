6427b819f3ec1d2cb937a8b47c00656fa0bbb17a
Joanna Rebelka <36218@student.ue.poznan.pl>
2016-03-23 12:58:09 +0100

    dodana funkcja get_info

--- calculator.py
+++ calculator.py
@@ -2,4 +2,10 @@ def dodawanie():
 	a = int(input("Podaj a"))
 	b = int(input("Podaj b"))
 	print(a+b)
+
+def get_info():
+	return "To jest program stworzony przez: Asia"
+
+print(get_info())
 dodawanie()
+input()	
